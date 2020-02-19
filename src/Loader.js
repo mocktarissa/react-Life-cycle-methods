b5ad9bd771b5332262a6e1a82cfa4bf9935bbe3a
Mocktar <mocktarissa@gmail.com>
2020-02-19 19:01:03 +0300

    Added a loading animation and changed the fonts

---
+++ src/Loader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './loader.css'
+class Loader extends React.Component{
+render(){
+    return <div>
+        <div class="load">
+ 
+    </div>
+    
+    <p>Loading <div class="btn">.</div>
+      
+    </p>
+    </div>
+}
+}
+
+export default Loader;
--- src/Zenquote.js
+++ src/Zenquote.js
@@ -1,31 +1,51 @@
 import React from 'react'
 import axios from 'axios';
+import Loader from './Loader'
+import './loader.css'
 
 class Zenquote extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            quote:""
+            quote:"",
+            isLoading:true
         }
     }
     componentDidMount(){
         axios.get("https://api.github.com/zen").then(res=>{
-            console.log(res.data);
-            this.setState({quote:res.data})
-        })
-
+           
+            setTimeout(
+                function(){
+                    this.setState({quote:res.data,isLoading:false})
+    
+                }.bind(this),3000
+            );
+    
+            
+        });
+       
     }
 
     render(){
-        return <div>
-           <h1>Hey dev always remember,</h1>
-           
-           <p>
-           "{this.state.quote}"    
-           </p>
+        
+
+return(
+    <div>
+    {
+    this.state.isLoading ? <Loader></Loader> :
+     <div>Hey dev , remember : <br/> <div className="quote"> 
+     " {this.state.quote}"
+         </div></div>
+    
+    }
+    </div>
+) 
+
+            
+            
            
 
-        </div>
+        
     }
 }
 
